7a789ebb1faac25641687df690104f0025986241
Mohamed Ismail Arif <mhdarif.vdm@gmail.com>
2020-04-26 07:36:02 +0530

    class design

---
+++ src/in/co/javacoder/oca/classdesign/superobjectclass/Zoo.java
@@ -0,0 +1,11 @@
+package in.co.javacoder.oca.classdesign.superobjectclass;
+
+public class Zoo extends ObjectClass /* extends java.lang.Object */  {
+	static {
+		System.out.println(Zoo.class.getSuperclass());
+	}
+	
+	 public static void main(String args[]) {
+		 
+	 }
+}
